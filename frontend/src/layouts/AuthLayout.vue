72296634495bea50d5e2f408011b8d5405e561af
Jerry Fang <jerryfang6606@gmail.com>
2026-05-12 22:23:35 +0800

    feat(auth): add user registration view

--- frontend/src/layouts/AuthLayout.vue
+++ frontend/src/layouts/AuthLayout.vue
@@ -3,7 +3,7 @@ defineOptions({ name: 'AuthLayout' })
 </script>
 
 <template>
-  <div class="flex min-h-screen items-center justify-center bg-bg px-4">
+  <div class="flex min-h-screen items-center justify-center bg-bg px-4 py-6">
     <RouterView />
   </div>
 </template>
